e0d3f66aebe9cd49f097003159630a48437865c5
Leonardo Freitas <leonardo.cadu2@hotmail.com>
2020-04-14 14:51:50 -0300

    feat sign in handleSubmit

--- src/components/signin/SignIn.jsx
+++ src/components/signin/SignIn.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../formInput/formInput";
 import CustomButtom from "../customButton/CustomButtom";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./styles.scss";
 
@@ -23,8 +23,18 @@ class SignIn extends React.Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+
+      this.setState({ email: "", password: "" });
+    } catch (err) {
+      console.error(err);
+    }
 
     this.setState({ email: "", password: "" });
   };
